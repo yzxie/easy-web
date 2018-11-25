edd60d260b6b92ad9c23e06ee5a2d6de84ee4669
xieyizun <xieyizun@tigerbrokers.com>
2018-11-25 18:55:02 +0800

    前端build压缩代码，具体参见easy-ui项目

---
+++ src/main/resources/static/precache-manifest.622f91f66df20ece7f0a1914b4929717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ec42c0b9771a5497c60",
+    "url": "/static/js/main.6ec42c0b.chunk.js"
+  },
+  {
+    "revision": "430030d811a750b67e59",
+    "url": "/static/js/1.430030d8.chunk.js"
+  },
+  {
+    "revision": "6ec42c0b9771a5497c60",
+    "url": "/static/css/main.20b7caa2.chunk.css"
+  },
+  {
+    "revision": "430030d811a750b67e59",
+    "url": "/static/css/1.4332a4da.chunk.css"
+  },
+  {
+    "revision": "87906b851720e06d813ef224b5c75c0d",
+    "url": "/index.html"
+  }
+];
